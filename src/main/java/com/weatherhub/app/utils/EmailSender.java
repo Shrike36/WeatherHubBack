35817b621c720866f70345c4fab820d91f917e5f
”Nikita <va123ma@gmail.com>
2021-10-09 00:51:35 +0300

    modifying trying to make heroku build success

--- src/main/java/com/weatherhub/app/utils/EmailSender.java
+++ src/main/java/com/weatherhub/app/utils/EmailSender.java
@@ -1,37 +1,34 @@
 package com.weatherhub.app.utils;
 
-import com.sendgrid.Response;
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
-import com.sendgrid.SendGrid;
-import com.sendgrid.Request;
-import com.sendgrid.Method;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 import java.io.IOException;
 
 public class EmailSender {
     public static void sendEmail(String email, String code) throws IOException {
-//        Email from = new Email("va123ma123@gmail.com");
-//        Email to = new Email(email); // use your own email address here
-//
-//        String subject = "WeatherHub Password Reset Code";
-//        Content content = new Content("text/html", code);
-//
-//        Mail mail = new Mail(from, subject, to, content);
-//
-//        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-//        Request request = new Request();
-//
-//        request.setMethod(Method.POST);
-//        request.setEndpoint("mail/send");
-//        request.setBody(mail.build());
-//
-//        Response response = sg.api(request);
-//
-//        System.out.println(response.getStatusCode());
-//        System.out.println(response.getHeaders());
-//        System.out.println(response.getBody());
+        Email from = new Email("va123ma123@gmail.com");
+        Email to = new Email(email); // use your own email address here
+
+        String subject = "WeatherHub Password Reset Code";
+        Content content = new Content("text/html", code);
+
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        Response response = sg.api(request);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
     }
 }
 
