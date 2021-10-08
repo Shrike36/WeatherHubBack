662bd6ad8d13657bbee11387acbfe570fdc8677c
”Nikita <va123ma@gmail.com>
2021-10-09 00:43:47 +0300

    modifying trying to make heroku build success

--- src/main/java/com/weatherhub/app/utils/EmailSender.java
+++ src/main/java/com/weatherhub/app/utils/EmailSender.java
@@ -12,26 +12,26 @@ import java.io.IOException;
 
 public class EmailSender {
     public static void sendEmail(String email, String code) throws IOException {
-        Email from = new Email("va123ma123@gmail.com");
-        Email to = new Email(email); // use your own email address here
-
-        String subject = "WeatherHub Password Reset Code";
-        Content content = new Content("text/html", code);
-
-        Mail mail = new Mail(from, subject, to, content);
-
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-        Request request = new Request();
-
-        request.setMethod(Method.POST);
-        request.setEndpoint("mail/send");
-        request.setBody(mail.build());
-
-        Response response = sg.api(request);
-
-        System.out.println(response.getStatusCode());
-        System.out.println(response.getHeaders());
-        System.out.println(response.getBody());
+//        Email from = new Email("va123ma123@gmail.com");
+//        Email to = new Email(email); // use your own email address here
+//
+//        String subject = "WeatherHub Password Reset Code";
+//        Content content = new Content("text/html", code);
+//
+//        Mail mail = new Mail(from, subject, to, content);
+//
+//        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+//        Request request = new Request();
+//
+//        request.setMethod(Method.POST);
+//        request.setEndpoint("mail/send");
+//        request.setBody(mail.build());
+//
+//        Response response = sg.api(request);
+//
+//        System.out.println(response.getStatusCode());
+//        System.out.println(response.getHeaders());
+//        System.out.println(response.getBody());
     }
 }
 
