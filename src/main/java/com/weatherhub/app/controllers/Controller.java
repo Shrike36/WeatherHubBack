9f33a3980ae1413702572e185729449c98a4b681
”Nikita <va123ma@gmail.com>
2021-10-09 01:15:36 +0300

    modifying trying to make heroku build success

--- src/main/java/com/weatherhub/app/controllers/Controller.java
+++ src/main/java/com/weatherhub/app/controllers/Controller.java
@@ -95,13 +95,6 @@ public class Controller {
                 : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
-
-    @PostMapping(value = "/users")
-    public ResponseEntity<?> create(@RequestBody User user) {
-        userService.create(user);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
     @GetMapping(value = "/users")
     public ResponseEntity<List<User>> read() {
         final List<User> users = userService.readAll();
@@ -109,32 +102,5 @@ public class Controller {
         return users != null &&  !users.isEmpty()
                 ? new ResponseEntity<>(users, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @GetMapping(value = "/users/{id}")
-    public ResponseEntity<User> read(@PathVariable(name = "id") int id) {
-        final User user = userService.read(id);
-
-        return user != null
-                ? new ResponseEntity<>(user, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @PutMapping(value = "/users/{id}")
-    public ResponseEntity<?> update(@PathVariable(name = "id") int id, @RequestBody User user) {
-        final boolean updated = userService.update(user, id);
-
-        return updated
-                ? new ResponseEntity<>(HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
-    }
-
-    @DeleteMapping(value = "/users/{id}")
-    public ResponseEntity<?> delete(@PathVariable(name = "id") int id) {
-        final boolean deleted = userService.delete(id);
-
-        return deleted
-                ? new ResponseEntity<>(HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 }
